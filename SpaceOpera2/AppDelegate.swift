490b01b998554f7f0a0c386387f56692a0eab8f6
Patrick <116662812+pwheelerRio@users.noreply.github.com>
2023-11-20 16:15:07 -0700

    Initial Commit

---
+++ SpaceOpera2/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SpaceOpera2
+//
+//  Created by Patrick Wheeler on 11/20/23.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
